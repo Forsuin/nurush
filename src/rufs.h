e01e1d95e85991c3f2c80c48ebfcca99e17a049e
Ian Laidlaw <ian@kaboodles.com>
2024-02-27 10:38:52 -0500

    New header for adding directories/files into fs

--- src/rufs.h
+++ src/rufs.h
@@ -1,73 +1,52 @@
-
-
 #include <string>
 #include <stack>
+#include <vector>
+#include <list>
 
 #include "variant.h"
 
-struct ProgramFile
+struct Program
 {
-    int cpu_req;
-    int mem_req;
+    int cpu;
+    int mem;
 };
 
-struct TextFile
+struct Text
 {
     std::string data;
-    int size;
 };
+
+struct Filable;
+struct Directory;
 
 struct Directory
 {
-    int size;
+    std::vector<Filable> contents;
+
+    Directory *parent;
+
+    Directory() : parent(nullptr) {}
+    Directory(Directory &parent) : parent(&parent) {}
 };
 
 struct Filable
 {
     char name[11];
 
-    variant<ProgramFile, TextFile, Directory> contents;
-};
-
-// For keeping track of where in binary file directory content count is
-struct DirIndexPair
-{
-    Filable dir;
-    int index;
+    variant<Program, Text, Directory> contents;
 };
 
 struct Filesystem
 {
-private:
-    std::stack<DirIndexPair> dirStack;
-
-public:
+    Directory root;
     std::string name;
 
-    /*
-    Writes a file into filesystem
-*/
     void write_file(Filable &file);
-
-    /*
-        Creates a new directory of the filesystem
-    */
     void create_dir(const std::string &dir_name);
-
-    /*
-        Closes most recent directory
-    */
     void end_dir();
-
-    /*
-        Closes any directories that are still open
-    */
     void close_dirs();
-
-    /*
-        Print out the contents of the filesystem
-    */
     void print();
 
-    Filesystem(const std::string &name) : name(name) {}
+    Filesystem() {}
+    Filesystem(std::string name) : name(name) {}
 };
